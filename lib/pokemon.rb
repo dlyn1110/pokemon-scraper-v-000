013aae892b7d7baf904a70c92ad5110f29db2af7
Dani Lyn <dlyn1110@gmail.com>
2018-07-03 03:28:48 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -20,7 +20,7 @@ class Pokemon
      pikachu
    end
 
-   def alter_hp(hp, db)
-     db.execute("UPDATE pokemon SET hp = @hp", hp)
-   end
+  #  def alter_hp(hp, db)
+  #    db.execute("UPDATE pokemon SET hp = @hp", hp)
+  #  end
 end
